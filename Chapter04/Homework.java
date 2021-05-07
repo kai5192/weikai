b522ed1296db43c6f0ddf6cbc21dbba5c2e6189e
weikai <kai5192@163.com>
2021-05-07 10:22:16 +0800

    第四章代码

--- Chapter04/Homework.java
+++ Chapter04/Homework.java
@@ -1,2 +1,32 @@
-package Chapter04;public class Homework {
+package Chapter04;
+public class Homework {
+    public static void main(String[] args){
+         int x1 = 10 / 3;
+         int x2 = 10 / 5;
+         double x3 = -10.5 % 3;
+        System.out.println("-------------------------作业一-------------------------------------");
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+        int i= 66;
+        System.out.println("-------------------------作业二-------------------------------------");
+        System.out.println(++i+i);// i = i +1 , temp = i, i = temp , 67+67
+        /*
+        int num1 = (int)"18";//错误 应该用Integer.parselInt("18");
+        int num2 = 18.0 ;//错 double > int
+        double num3 = 3d; // ok
+        double num = 8; // ok
+        int i1 = 48; char ch = i1 +1; //错 int > char
+        byte b = 19; short s = b+ 2; // int > short
+
+         */
+        String str = "18.8";
+        double d1 = Double.parseDouble(str);
+        char c1 = '韩';
+        String str2 = c1 +"";
+        System.out.println("-------------------------作业四-------------------------------------");
+        System.out.println(d1);
+        System.out.println(c1);
+        System.out.println(str2);
+    }
 }
--- Chapter04/Input.java
+++ Chapter04/Input.java
@@ -1,2 +1,22 @@
-package Chapter04;public class Input {
+package Chapter04;
+import java.util.Scanner;
+public class Input {
+    //编写一个main方法
+    public static void main(String[] args){
+        //演示接受用户的输入
+        //1. 引入Scanner类所在的包
+        //2. 创建Scanner对象,new创建一个对象
+        Scanner myscanner = new Scanner(System.in);
+        //3. 接收用户的输入
+        System.out.println("请输入你的名字");
+        String name = myscanner.next();//接受用户的输入
+        System.out.println("请输入你的年龄");
+        int age = myscanner.nextInt();//接受用户的输入
+        System.out.println("请输入你的薪水");
+        double sal = myscanner.nextDouble();//接受用户的输入
+        System.out.println("name = " + name + "\n" +
+                "年龄 = " + age + "\n" + "薪水" + sal);
+
+
+    }
 }
